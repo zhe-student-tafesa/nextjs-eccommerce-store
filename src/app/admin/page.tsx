db7401c8c6e24c969fba6f4d604bc165a969677c
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-10-02 12:58:30 +1000

    1. Use Card: from shadcn

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,24 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 const AdminDashboard = () => {
+    //  grid: Enable CSS Grid Layout. Using the grid class turns an element into a grid container.
+    // grid-cols-1: On small screens, the grid container will have 1 column. This is the default behavior and is suitable for mobile phones or small screen devices.
+
+    // md:grid-cols-2: On medium screens (md means screen width ≥ 768px), the grid container will have 2 columns. When the screen width reaches the medium breakpoint, the layout automatically switches to 2 columns.
+    // lg:grid-cols-3: On large screens (lg means screen width ≥ 1024px), the grid container will have 3 columns. When the screen reaches the larger breakpoint, the layout adjusts to 3 columns.
     return (
-        <div>AdminDashboard</div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Sales</CardTitle>
+                    <CardDescription>desc</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>text</p>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
