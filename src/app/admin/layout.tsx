419c0874abb0990bfdf35feec40a7b2d7ae22e77
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-10-03 13:25:54 +1000

    Tell nextjs not to cash any of admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
 import Nav, { NavLink } from "@/components/nav/Nav";
 
+///  Tell nextjs not to cash any of admin page!!
+export const dynamic = "force-dynamic"
+
 //  admin page layout with navigation links
 export default function AdminLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
