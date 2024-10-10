ac348346a97a452d9bf45e9449810ceb8edb442a
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-10-10 13:12:58 +1000

    1. show form error; 2. when create product: isAvailableForPurchase: false 3. After click Save button: use pending Status to disable Save Button;

--- src/components/productForm/ProductForm.tsx
+++ src/components/productForm/ProductForm.tsx
@@ -6,17 +6,20 @@ import { formatCurrency } from '@/lib/formatters'
 import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
 import { addProduct } from '@/lib/actions/action_admin'
+import { useFormState, useFormStatus } from 'react-dom'
 
 const ProductForm = () => {
     const [priceInCents, setPriceInCents] = useState<number>()
-
+    const [error, action] = useFormState(addProduct, {})
     return (
-        <form action={addProduct} className='space-y-8'>
+        <form action={action} className='space-y-8'>
             <div className='space-y-2'>
                 <Label htmlFor='name'>Name</Label>
                 <Input type='text' id='name' name='name' required>
                 </Input>
+                {error.name && <div className='text-destructive'>{error.name}</div>}
             </div>
+
 
             <div className='space-y-2'>
                 <Label htmlFor='priceInCents'>Price In Cents</Label>
@@ -32,28 +35,38 @@ const ProductForm = () => {
                 <div className='text-muted-foreground'>
                     {formatCurrency((priceInCents || 0) / 100)}
                 </div>
+                {error.priceInCents && <div className='text-destructive'>{error.priceInCents}</div>}
             </div>
 
             <div className='space-y-2'>
                 <Label htmlFor='description'>Description</Label>
                 <Textarea id='description' name='description' required />
+                {error.description && <div className='text-destructive'>{error.description}</div>}
             </div>
 
             <div className='space-y-2'>
                 <Label htmlFor='file'>File</Label>
                 <Input type='file' id='file' name='file' required>
                 </Input>
+                {error.file && <div className='text-destructive'>{error.file}</div>}
             </div>
 
             <div className='space-y-2'>
                 <Label htmlFor='image'>Image</Label>
                 <Input type='file' id='image' name='image' required>
                 </Input>
+                {error.image && <div className='text-destructive'>{error.image}</div>}
             </div>
 
-            <Button type='submit'>Save</Button>
+            <SubmitButton />
         </form>
     )
 }
 
 export default ProductForm
+
+// use pending Status to disable Save Button
+function SubmitButton() {
+    const { pending } = useFormStatus()
+    return <Button type='submit' disabled={pending}>{pending ? 'Saving...' : "Save"}</Button>
+}
--- src/lib/actions/action_admin.ts
+++ src/lib/actions/action_admin.ts
@@ -22,7 +22,7 @@ const addSchema = z.object({
     file: fileSchema.refine(file => file.size > 0, 'Required'),
     image: imageSchema.refine(file => file.size > 0, 'Required')
 })
-export async function addProduct(formData: FormData) {
+export async function addProduct(previousState:unknown, formData: FormData) {
     // console.log(formData)
     const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
     if (result.success === false) {
@@ -45,6 +45,7 @@ export async function addProduct(formData: FormData) {
 
     await db.product.create({
         data: {
+            isAvailableForPurchase: false,
             name: data.name,
             description: data.description,
             priceInCents: data.priceInCents,
