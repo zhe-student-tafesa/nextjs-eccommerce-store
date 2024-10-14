57df1d5d00e63cbdd03afdefed5dfc2101db378f
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-10-14 13:08:24 +1000

    1. .env: add  ADMIN_USER_NAME and ADMIN_HASHED_PASSWORD 2. use  ADMIN_USER_NAME and ADMIN_HASHED_PASSWORD to check auth;

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 
 import { NextRequest, NextResponse } from "next/server";
+import { isValidPassword } from "./lib/isValidPassword";
 
 export async function middleware(req: NextRequest) {
     if ((await isAuthenticated(req)) === false) {
@@ -15,7 +16,20 @@ export async function middleware(req: NextRequest) {
 }
 
 async function isAuthenticated(req: NextRequest) {
-    return Promise.resolve(false)
+    //return Promise.resolve(false)
+    const authHeader = req.headers.get("authorization") || req.headers.get("Authorization")
+    if (authHeader == null)
+        return Promise.resolve(false)
+
+    // decrypt: username password
+    console.log("authHeader:", authHeader) //  Basic MTIzNDU6YWFiYg==
+    // so index of MTIzNDU6YWFiYg==  is 1: authHeader.split(" ")[1]
+    //  username:password, so use .split(":")
+    const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":")
+    console.log("username:", username)
+    console.log("password:", password)
+    return username === process.env.ADMIN_USER_NAME &&
+        (await isValidPassword(password, process.env.ADMIN_HASHED_PASSWORD))
 }
 
 
