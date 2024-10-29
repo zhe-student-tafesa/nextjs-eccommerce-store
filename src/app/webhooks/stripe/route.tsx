8d88a2797f607a952cc813a781c6c4000b818cd5
frankzhang <frank.zhang.sa.au@gmail.com>
2024-10-29 20:52:00 +1000

    1. see stripeData.json 2. So get real email and amount！！

--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -18,62 +18,80 @@ export async function POST(req: NextRequest) {
   // verify the info from STRIPE
   // stripe-signature : be used to compare against our actual web hook
 
-  const stripeSignature = req.headers.get("stripe-signature") as string;
-  console.log("2024Stripe signature:", stripeSignature);
+  // const rawBody = await req.text();
+  // console.log("Raw Body1029:", rawBody);
 
-  const event = await stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get("stripe-signature") as string,
-    process.env.STRIPE_WEBHOOK_SECRET as string
-  );
-  if (event.type === "charge.succeeded") {
-    const charge = event.data.object;
-    // prepare all the info to create an order
-    const productId = charge.metadata.productId;
-    const email = charge.metadata.email;
-    // MUST　ＢＥ　Int
-    const pricePaidInCents = parseInt(charge.metadata.amout);
-    // double check is this product exist
-    const product = await db.product.findUnique({ where: { id: productId } });
-    if (product == null || email == null) {
-      // let stripe know: this is not what we expect
-      return new NextResponse("Bad Request", { status: 400 });
+  // const stripeSignature = req.headers.get("stripe-signature") as string;
+  // console.log("2024Stripe signature:", stripeSignature);
+
+  try {
+    const event = await stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+    // console.log("event1029", event);
+    if (event.type === "charge.succeeded") {
+      console.log("event1029:", event);
+      const charge = event.data.object;
+      // console.log("charge.metadata:", charge.metadata);
+      // prepare all the info to create an order
+      const productId = charge.metadata.productId;
+
+      // ！！！must see stripeData.json
+      const email = charge.billing_details.email;
+      console.log("productId-:", productId);
+      console.log("email-:", email);
+
+      // ！！！must see stripeData.json
+      const pricePaidInCents = charge.amount;
+      // double check is this product exist
+      const product = await db.product.findUnique({ where: { id: productId } });
+      if (product == null || email == null) {
+        // let stripe know: this is not what we expect
+        return new NextResponse("Bad Request", { status: 400 });
+      }
+      // create or update this user:  where: { email },
+      // if email not exist,  create
+      // else,  update
+      const userFields = {
+        email,
+        orders: { create: { productId, pricePaidInCents } },
+      };
+      // {orders}： Object Destructuring
+      // orders: [order] means to take out the first element in the orders array and name it order, so that you can use order directly without accessing orders[0] every time.
+      const {
+        orders: [order],
+      } = await db.user.upsert({
+        where: { email },
+        create: userFields,
+        update: userFields,
+        // after finish: read order
+        select: { orders: { orderBy: { createdAt: "desc" }, take: 1 } },
+      });
+
+      // 14 days
+      const downloadVerification = await db.downloadVerification.create({
+        data: {
+          productId,
+          expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
+        },
+      });
+      // after create: send user an email like receipt
+      // receipt: order + downloadVerification
+      // use resend
+      await resend.emails.send({
+        from: `Support <${process.env.SENDER_EMAIL}>`,
+        subject: "Order Confirmation",
+        to: email,
+        react: <h1>Hi </h1>,
+      });
+      console.log("event1029-real success");
     }
-    // create or update this user:  where: { email },
-    // if email not exist,  create
-    // else,  update
-    const userFields = {
-      email,
-      orders: { create: { productId, pricePaidInCents } },
-    };
-    // {orders}： Object Destructuring
-    // orders: [order] means to take out the first element in the orders array and name it order, so that you can use order directly without accessing orders[0] every time.
-    const {
-      orders: [order],
-    } = await db.user.upsert({
-      where: { email },
-      create: userFields,
-      update: userFields,
-      // after finish: read order
-      select: { orders: { orderBy: { createdAt: "desc" }, take: 1 } },
-    });
-
-    // 14 days
-    const downloadVerification = await db.downloadVerification.create({
-      data: {
-        productId,
-        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 14),
-      },
-    });
-    // after create: send user an email like receipt
-    // receipt: order + downloadVerification
-    // use resend
-    await resend.emails.send({
-      from: `Support <${process.env.SENDER_EMAIL}>`,
-      subject: "Order Confirmation",
-      to: email,
-      react: <h1>Hi </h1>,
-    });
+    return NextResponse.json({ received: true }, { status: 200 });
+  } catch (error: unknown) {
+    const message = (error as Error).message; //
+    console.error(`Webhook Error-catch1029: ${message}`);
+    return NextResponse.json({ error: "Webhook Error 1029" }, { status: 400 });
   }
-  return new NextResponse();
 }
